e17ac48fdd6a4cba2e433646fea6c52f231ff28b
AmineBotza <botza.amine@gmail.com>
2023-11-17 01:54:02 +0100

    starting all over

--- main.h
+++ main.h
@@ -1,11 +1,23 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include<stdarg.h>
 #include<stdlib.h>
 #include<unistd.h>
 
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} match;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
-int putss(char *c);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
+int print_dec(va_list args);
+int print_int(va_list args);
 
 #endif
---
+++ print_str.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+  * _strlen - returns the length of a string
+  *@str: string pointer
+  *Return: 1
+  */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+		return (i);
+}
+
+/**
+  * _strlenc - strlen function applied to constant
+  *@str: char point
+  *Return: 1.
+  */
+
+int _strlenc(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	return (i);
+}
